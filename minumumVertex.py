4860cbc757d14f6620332c82116c82021432eaf2
António Ramos <tonyantonio@outlook.pt>
2021-11-13 18:22:52 +0000

    fix generate graph

--- minumumVertex.py
+++ minumumVertex.py
@@ -1,3 +1,5 @@
+from os import write
+from itertools import combinations, permutations
 import time
 import random
 
@@ -11,45 +13,84 @@ def generate_coordinate(min=1, max=9):
        x, y = generate_coordinate(min, max)
     return x, y
 
+def generate_edges(vertices):
+    graph = {}
+   
     
 def generate_vertices(n_vertices):
     vertices = []
-    sum_edge = 0
+   
+    #print(list(generate_coordinate()))
     for i in range(n_vertices):
-        vertices.append(generate_coordinate())
-    graph = {} 
-    for j in vertices:
-        num_edge = random.randint(0, n_vertices)
-        sum_edge += num_edge
-        if sum_edge <= n_vertices:
-            graph[j] = num_edge
-        else: 
-            graph[j] = 0
-    return graph
+       vertices.append(generate_coordinate())
 
+    return vertices
+
+#function to create a new file when the application runs
 def create_file():
     file = open(text_file, "w")  
     file.write("id: {(x, y): n_edges}\n")
     file.close()
 
+#function to write information of graph on the file
 def write_file(id, graph):
     file = open(text_file, "a")  
-    file.write(str(id) + ": " + str(graph) + "\n")
+    file.write(str(id) + ": " + str(graph) + "\n\n")
     file.close() 
+
+#function to write the percentage of edges on file before writing graph    
+def write_percentage(percentage):
+    file = open(text_file, "a")
+    file.write("\nPercentage of egdes " + str(percentage) + "% \n")
+    file.close()
+
+#convert graph into adj_matrix and set edges
+def make_adj_matrix(graph):
+    for id in graph: # loop through id vertices 
+        row = []
+        for point in graph[id]: # loop to get the points generated previous 
+            row.append(point)
+            col = []
+            for point2 in graph[id]:
+                col.append(point2)
+            adj_matrix = [row, col]
+        print(adj_matrix)
+
+    for row in adj_matrix:
+        print(row)
+        for col in row:
+            print(col)
+
+
     
 
 def main():
     count_operations = 0
+    graph = {}
+    percentage_egdes = [0, 25, 50, 75, 100]
+    min_vertices = 2
+    max_vertices = 10
     create_file()
     start = time.time()
-
-    for i in range(2,26):
-        graph = generate_vertices(i)
-        print(graph)
-        write_file(i, graph)
+    
+    for i in range(min_vertices,max_vertices+1):
+        vert = generate_vertices(i)
+        graph[i] = [[point for point in vert] for point in vert]
+        #print(graph)
         stop = time.time() - start
         print("Time generated " + str(stop))
 
+    for percentage in percentage_egdes:
+        write_percentage(percentage)
+        for i in graph:
+            write_file(i, graph[i])
+            #print(graph[i])
+    
+
+    #make_adj_matrix(graph)
+    
+    #print(adj_matrix)
+        
 
 if __name__ == '__main__':
     main()
