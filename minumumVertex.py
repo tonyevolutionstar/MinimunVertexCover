0c20584404199ba6a30c757e1988b065473e3e6d
António Ramos <tonyantonio@outlook.pt>
2021-11-25 17:08:14 +0000

    add function test minimum vertex
    Test with another function

--- minumumVertex.py
+++ minumumVertex.py
@@ -17,33 +17,21 @@ def generate_coordinate(min=1, max=9):
        x, y = generate_coordinate(min, max)
     return x, y
 
-def generate_edges(id, vertices, percentage):
+def generate_edges(vertices, percentage):
     adj_matrix = [[0 for i in range(len(vertices))] for j in range(len(vertices))]
     size_graph = len(vertices)
 
-    # because 2 is 
-    if id == 2: 
+    number_edges = round(size_graph * (percentage/100))
+
+    number_of_alt = 0 # operations to change random vertice to fill the percentage of edges
+    if number_edges != 0:
         for i in range(size_graph):
             for j in range(size_graph):
-                if vertices[i] != vertices[j]: 
-                    adj_matrix[i][j] = 1
-                else:
-                    adj_matrix[i][j] = 0
-    else:
-        number_edges = round(size_graph * (percentage/100))
-
-        number_of_alt = 0 # operations to change random vertice to fill the percentage of edges
-        if number_edges != 0:
-            for i in range(size_graph):
-                for j in range(size_graph):
-                    if number_of_alt <= number_edges:
-                        vert_i = random.choice(vertices[i])
-                        vert_j = random.choice(vertices[j])
-                        number_of_alt += 1
-                        adj_matrix[vert_i][vert_j] = 1
-    
-
-
+                if number_of_alt <= number_edges:
+                    vert_i = random.choice(vertices[i])
+                    vert_j = random.choice(vertices[j])
+                    number_of_alt += 1
+                    adj_matrix[vert_i][vert_j] = 1
                 
     return adj_matrix
    
@@ -90,39 +78,84 @@ def write_adj_matrix(id, graph):
     file.write("\t\n")
     file.close() 
 
+def exaustive_search_vertex(graph):
+    """ 
+        THIS CODE WAS ADAPTED FROM
+        https://www.geeksforgeeks.org/vertex-cover-problem-set-1-introduction-approximate-algorithm-2/
+    """
+    # Initialize all vertices as not visited.
+    visited = [False for i in range(len(graph))]
+
+    for node in range(len(graph)):
+        # An edge is only picked when
+        # both visited[u] and visited[v]
+        # are false
+        if not visited[node]:
+            # Go through all adjacents of u and
+            # pick the first not yet visited
+            # vertex (We are basically picking
+            # an edge (u, v) from remaining edges.
+            for v in range(len(graph)):
+                if not visited[node]:   
+                    # Add the vertices (u, v) to the
+                    # result set. We make the vertex
+                    # u and v visited so that all
+                    # edges from/to them would
+                    # be ignored
+                    visited[v] = True
+                    visited[node] = True
+                    break
+    """
+        # Print the vertex cover
+    for node in range(len(graph)):
+        if visited[node]:
+            print(node, end = ' ')
+    """
+    return visited
+
+
+
 def main():
     count_operations = 0
     graph = {}
-    percentage_egdes = [25, 50, 75, 100]
+    percentage_egdes = [25, 50, 75]
     min_vertices = 10
-    max_vertices = 20
+    max_vertices = 11
     create_file() # create "output_graph.txt" every time that program runs
     create_file_adj() # for adj matrix
 
     start = time.time()
-    
-    # generate graph one time and save the graph on dictionary 
-    for i in range(min_vertices,max_vertices+1):
-        graph[i] = generate_vertices(i)
-        stop = time.time() - start
-        print("Graph " + str(i) + " generated in " + str(round(stop,5)) + " seconds")
 
-    # generate edges and adj_matrix
-    for percentage in percentage_egdes:
-        write_percentage(text_file, percentage)
-        write_percentage(adj_file, percentage)
-        for i in graph:
-            points = graph[i]
-            write_file(i, points) # write to a file "output_graph.txt" the graph
-            
-            # initializate the adj matrix 
-            #adj_matrix = [[0 for i in points] for j in points]
-            adj_matrix = generate_edges(i, points, percentage)
+    if min_vertices == max_vertices:
+        print("The max and the min can't be equal")
+        exit(1)
 
+    if min_vertices >= 10:
+        # generate graph one time and save the graph on dictionary 
+        for i in range(min_vertices,max_vertices+1):
+            graph[i] = generate_vertices(i)
+            stop = time.time() - start
+            print("Graph " + str(i) + " generated in " + str(round(stop,5)) + " seconds")
 
-            write_adj_matrix(i, adj_matrix)
+        # generate edges and adj_matrix
+        for percentage in percentage_egdes:
+            write_percentage(adj_file, percentage)
+            for i in graph:
+                points = graph[i]
+                write_file(i, points) # write to a file "output_graph.txt" the graph
+                # initializate the adj matrix 
+                adj_matrix = generate_edges(points, percentage)
+                exaustive = exaustive_search_vertex(points)
+                print("Minimum Vertex Cover Set")
+                for node in range(len(points)):
+                    if exaustive[node]:
+                        print(node, end = ' ')
+                write_adj_matrix(i, adj_matrix)
+    else:
+        print("The program only accepts graph with 10 or more vertices")
+        exit(1)
 
+  
         
-
 if __name__ == '__main__':
     main()
